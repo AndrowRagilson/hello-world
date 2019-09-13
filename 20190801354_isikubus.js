ec6f1795a2902ed30ab65fdea93c19d1cb4ebc32
Androw Ragilson Soleh Aji Davisga <54989439+AndrowRagilson@users.noreply.github.com>
2019-09-13 23:06:52 +0700

    Add files via upload

---
+++ 20190801354_isikubus.js
@@ -0,0 +1,9 @@
+console.log("TUGAS ISI KUBUS");
+
+var insertkey = require("readline-sync");
+
+var sisi =  parseInt(insertkey.question("Masukan Sisi sisi : "));
+
+var isi = sisi*sisi*sisi;
+
+console.log("isi kubus : "+isi);
